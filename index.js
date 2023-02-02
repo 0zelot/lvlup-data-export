9b5b8198311c49a38be86bfbc52913e08d2e99f4
Ozelot <29740364+0zelot@users.noreply.github.com>
2023-02-02 21:02:31 +0100

    More data export

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const chalk = require("chalk");
 
 const fs = require("node:fs/promises");
 
-const {getMainInfo, getAccountLogs, getAccountTickets, getTicketInfo} = require("./functions.js");
+const {getMainInfo, getAccountLogs, getAccountTickets, getTicketInfo, getWalletInfo, getAccountPayments, getAccountOrders, getReferralInfo} = require("./functions.js");
 
 const dir = "./lvlup-data-export"
 
@@ -15,8 +15,15 @@ const token = rl.question(`${chalk.cyanBright("Enter your auth token: ")}`, {hid
 
     fs.mkdir(dir).catch(err => false);
 
-    fs.writeFile(`${dir}/me.json`, JSON.stringify(await getMainInfo(token), null, 2));
+    const mainInfo = await getMainInfo(token);
+    if(!mainInfo.uid) return console.error(chalk.redBright("Authorization failed!"), mainInfo);
+
+    fs.writeFile(`${dir}/me.json`, JSON.stringify(mainInfo, null, 2));
     fs.writeFile(`${dir}/logs.json`, JSON.stringify(await getAccountLogs(token), null, 2));
+    fs.writeFile(`${dir}/wallet.json`, JSON.stringify(await getWalletInfo(token), null, 2));
+    fs.writeFile(`${dir}/payments.json`, JSON.stringify(await getAccountPayments(token), null, 2));
+    fs.writeFile(`${dir}/orders.json`, JSON.stringify(await getAccountOrders(token), null, 2));
+    fs.writeFile(`${dir}/referral.json`, JSON.stringify(await getReferralInfo(token), null, 2));
 
     fs.mkdir(`${dir}/tickets`).catch(err => false);
 
@@ -26,6 +33,6 @@ const token = rl.question(`${chalk.cyanBright("Enter your auth token: ")}`, {hid
     for(const ticket in tickets.items) 
         fs.writeFile(`${dir}/tickets/${tickets.items[ticket].id}.json`, JSON.stringify(await getTicketInfo(token, tickets.items[ticket].id), null, 2));
 
-    console.log("Gotowe")
+    console.log(chalk.greenBright("Data exported successfully."));
     
 })();
