16666985ad135bb932544f3622ca84de5a63d982
Ozelot <29740364+0zelot@users.noreply.github.com>
2023-02-02 21:01:50 +0100

    Added some new functions

--- functions.js
+++ functions.js
@@ -40,4 +40,45 @@ const getTicketInfo = async (token, id) => await (await fetch(`https://api.lvlup
 }})).json()
 
 
-module.exports = {getMainInfo, getAccountLogs, getAccountTickets, getTicketInfo}
+const getWalletInfo = async (token) => await (await fetch("https://api.lvlup.pro/v4/wallet", {
+    headers: {
+        "Authorization": `Bearer ${token}`
+}})).json()
+
+
+const getAccountPayments = async (token) => {
+
+    const count = (await (await fetch("https://api.lvlup.pro/v4/payments?limit=10&afterId=0", {
+        headers: {
+            "Authorization": `Bearer ${token}`
+    }})).json()).count;
+
+    return await (await fetch(`https://api.lvlup.pro/v4/payments?limit=${count}&afterId=0`, {
+        headers: {
+            "Authorization": `Bearer ${token}`
+    }})).json()
+
+}
+
+const getAccountOrders = async (token) => {
+
+    const count = (await (await fetch("https://api.lvlup.pro/v4/orders?limit=10&afterId=0", {
+        headers: {
+            "Authorization": `Bearer ${token}`
+    }})).json()).count;
+
+    return await (await fetch(`https://api.lvlup.pro/v4/orders?limit=${count}&afterId=0`, {
+        headers: {
+            "Authorization": `Bearer ${token}`
+    }})).json()
+
+}
+
+
+const getReferralInfo = async (token) => await (await fetch("https://api.lvlup.pro/v4/me/referral", {
+    headers: {
+        "Authorization": `Bearer ${token}`
+}})).json()
+
+
+module.exports = {getMainInfo, getAccountLogs, getAccountTickets, getTicketInfo, getWalletInfo, getAccountPayments, getAccountOrders, getReferralInfo}
